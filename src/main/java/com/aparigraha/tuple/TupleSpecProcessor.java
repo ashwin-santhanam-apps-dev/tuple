57197b8fcad61613f492780ba18d0847f06d6275
AshwinSanthanam <ashwinsanthanam098@gmail.com>
2026-01-15 16:37:16 +0530

    Use integrate pebble with Annotation Processor

--- src/main/java/com/aparigraha/tuple/TupleSpecProcessor.java
+++ src/main/java/com/aparigraha/tuple/TupleSpecProcessor.java
@@ -1,5 +1,9 @@
 package com.aparigraha.tuple;
 
+import io.pebbletemplates.pebble.PebbleEngine;
+import io.pebbletemplates.pebble.loader.ClasspathLoader;
+import io.pebbletemplates.pebble.template.PebbleTemplate;
+
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
@@ -10,20 +14,17 @@ import javax.tools.Diagnostic;
 import javax.tools.JavaFileObject;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Set;
-import java.util.function.Predicate;
 
 
 @SupportedAnnotationTypes("com.aparigraha.tuple.TupleSpec")
 @SupportedSourceVersion(SourceVersion.RELEASE_21)
 public class TupleSpecProcessor extends AbstractProcessor {
-    private final TupleSchemaGenerator tupleSchemaGenerator = new TupleSchemaGenerator(
-            "com.aparigraha.tuple",
-            "Tuple",
-            "T",
-            "item"
-    );
+    private static final PebbleTemplate template = getTemplate();
 
 
     @Override
@@ -33,29 +34,53 @@ public class TupleSpecProcessor extends AbstractProcessor {
                 .map(element -> element.getAnnotation(TupleSpec.class))
                 .flatMap(tupleSpec -> Arrays.stream(tupleSpec.value()).boxed())
                 .distinct()
-                .map(tupleSchemaGenerator::generate)
-                .map(this::saveTupleSchema)
-                .allMatch(Predicate.isEqual(true));
+                .allMatch(size -> saveTupleSchema(generateTuple(size), size));
     }
 
 
-    private boolean saveTupleSchema(TupleSchema tupleSchema) {
+    private static PebbleTemplate getTemplate() {
+        ClasspathLoader loader = new ClasspathLoader();
+        loader.setPrefix("templates");
+        PebbleEngine engine = new PebbleEngine.Builder().strictVariables(true).loader(loader).build();
+        return engine.getTemplate("Tuple.peb");
+    }
+
+
+    private String generateTuple(int size) {
+        Writer writer = new StringWriter();
+        try {
+            template.evaluate(writer, Map.of("size", size));
+            return writer.toString();
+        } catch (IOException exception) {
+            processingEnv.getMessager().printMessage(
+                    Diagnostic.Kind.ERROR,
+                    "Failed creating Tuple with size: " + size + "\n" + exception.getMessage()
+            );
+        }
+        return null;
+    }
+
+
+    private boolean saveTupleSchema(String tupleSchema, int size) {
+        if (tupleSchema == null) {
+            return false;
+        }
         try {
             JavaFileObject file = processingEnv
                     .getFiler()
-                    .createSourceFile(tupleSchema.fullyQualifiedClassName());
+                    .createSourceFile("com.aparigraha.tuples.Tuple" + size);
             try (PrintWriter out = new PrintWriter(file.openWriter())) {
-                out.print(tupleSchema.textContent());
+                out.print(tupleSchema);
             }
             processingEnv.getMessager().printMessage(
                     Diagnostic.Kind.NOTE,
-                    "Created Tuple class: " + tupleSchema.className()
+                    "Created Tuple class with size: " + size
             );
             return true;
         } catch (IOException exception) {
             processingEnv.getMessager().printMessage(
                     Diagnostic.Kind.ERROR,
-                    "Failed creating Tuple class: " + tupleSchema.className() + "\n" + exception.getMessage()
+                    "Failed creating Tuple class with size: " + size + "\n" + exception.getMessage()
             );
             return false;
         }
