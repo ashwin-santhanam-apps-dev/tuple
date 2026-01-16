3da76d1c827ea195f354b91a94fa88635db02ff6
AshwinSanthanam <ashwinsanthanam098@gmail.com>
2026-01-16 23:52:41 +0530

    Enhance test for TupleSpecProcessor - Check for optimised generation in case of tuple spec redundancy

--- src/test/java/com/aparigraha/tuple/TupleSpecProcessorTest.java
+++ src/test/java/com/aparigraha/tuple/TupleSpecProcessorTest.java
@@ -96,4 +96,70 @@ class TupleSpecProcessorTest {
 
         assertTrue(tupleSpecProcessor.process(Set.of(), roundEnvironment));
     }
+
+
+    @Test
+    void shouldGenerateTuplesForUniqueSizesWhenAnnotationsAreAreDefinedAcrossMultipleElements() throws IOException {
+        var tupleSpec1 = mock(TupleSpec.class);
+        when(tupleSpec1.value())
+                .thenReturn(new int[] {2, 4, 1, 3});
+
+        var tupleSpec2 = mock(TupleSpec.class);
+        when(tupleSpec2.value())
+                .thenReturn(new int[] {1, 3, 2, 4});
+
+        var element1 = mock(VariableElement.class);
+        when(element1.getAnnotation(TupleSpec.class)).thenReturn(tupleSpec1);
+
+        var element2 = mock(VariableElement.class);
+        when(element2.getAnnotation(TupleSpec.class)).thenReturn(tupleSpec2);
+
+        when(tupleGenerator.generate(any())).thenAnswer(invocation -> {
+            TupleGenerationParams params = invocation.getArgument(0);
+
+            if ("Tuple4".equals(params.className()) &&
+                    "com.aparigraha.tuples".equals(params.packageName()) &&
+                    Objects.equals(params.fields(), List.of("item0", "item1", "item2", "item3"))
+            ) {
+                return "Tuple code for Tuple4";
+            }
+            else if ("Tuple2".equals(params.className()) &&
+                    "com.aparigraha.tuples".equals(params.packageName()) &&
+                    Objects.equals(params.fields(), List.of("item0", "item1"))
+            ) {
+                return "Tuple code for Tuple2";
+            }
+            else if ("Tuple3".equals(params.className()) &&
+                    "com.aparigraha.tuples".equals(params.packageName()) &&
+                    Objects.equals(params.fields(), List.of("item0", "item1", "item2"))
+            ) {
+                return "Tuple code for Tuple3";
+            }
+            else if ("Tuple1".equals(params.className()) &&
+                    "com.aparigraha.tuples".equals(params.packageName()) &&
+                    Objects.equals(params.fields(), List.of("item0"))
+            ) {
+                return "Tuple code for Tuple1";
+            }
+            else return null;
+        });
+
+        doReturn(Set.of(element1, element2))
+                .when(roundEnvironment)
+                .getElementsAnnotatedWith(TupleSpec.class);
+
+
+        when(tupleSchemaWriter.write(eq("Tuple code for Tuple4"), eq("com.aparigraha.tuples.Tuple4"), any()))
+                .thenReturn(true);
+        when(tupleSchemaWriter.write(eq("Tuple code for Tuple2"), eq("com.aparigraha.tuples.Tuple2"), any()))
+                .thenReturn(true);
+        when(tupleSchemaWriter.write(eq("Tuple code for Tuple3"), eq("com.aparigraha.tuples.Tuple3"), any()))
+                .thenReturn(true);
+        when(tupleSchemaWriter.write(eq("Tuple code for Tuple1"), eq("com.aparigraha.tuples.Tuple1"), any()))
+                .thenReturn(true);
+
+        TupleSpecProcessor tupleSpecProcessor = new TupleSpecProcessor(tupleGenerator, tupleSchemaWriter);
+
+        assertTrue(tupleSpecProcessor.process(Set.of(), roundEnvironment));
+    }
 }
