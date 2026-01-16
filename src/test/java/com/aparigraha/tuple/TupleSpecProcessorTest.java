ba142cbf5b25935a21847cc0b28b16491df95dfa
AshwinSanthanam <ashwinsanthanam098@gmail.com>
2026-01-16 23:50:08 +0530

    Enhance test for TupleSpecProcessor - Multiple tuples in single annotation for multiple elements

--- src/test/java/com/aparigraha/tuple/TupleSpecProcessorTest.java
+++ src/test/java/com/aparigraha/tuple/TupleSpecProcessorTest.java
@@ -9,18 +9,14 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import javax.annotation.processing.Filer;
 import javax.annotation.processing.RoundEnvironment;
-import javax.lang.model.element.Element;
 import javax.lang.model.element.VariableElement;
-import javax.tools.JavaFileObject;
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.*;
 
 
@@ -42,8 +38,15 @@ class TupleSpecProcessorTest {
         when(tupleSpec1.value())
                 .thenReturn(new int[] {2, 4});
 
+        var tupleSpec2 = mock(TupleSpec.class);
+        when(tupleSpec2.value())
+                .thenReturn(new int[] {1, 3});
+
         var element1 = mock(VariableElement.class);
         when(element1.getAnnotation(TupleSpec.class)).thenReturn(tupleSpec1);
+
+        var element2 = mock(VariableElement.class);
+        when(element2.getAnnotation(TupleSpec.class)).thenReturn(tupleSpec2);
 
         when(tupleGenerator.generate(any())).thenAnswer(invocation -> {
             TupleGenerationParams params = invocation.getArgument(0);
@@ -60,10 +63,22 @@ class TupleSpecProcessorTest {
             ) {
                 return "Tuple code for Tuple2";
             }
+            else if ("Tuple3".equals(params.className()) &&
+                    "com.aparigraha.tuples".equals(params.packageName()) &&
+                    Objects.equals(params.fields(), List.of("item0", "item1", "item2"))
+            ) {
+                return "Tuple code for Tuple3";
+            }
+            else if ("Tuple1".equals(params.className()) &&
+                    "com.aparigraha.tuples".equals(params.packageName()) &&
+                    Objects.equals(params.fields(), List.of("item0"))
+            ) {
+                return "Tuple code for Tuple1";
+            }
             else return null;
         });
 
-        doReturn(Set.of(element1))
+        doReturn(Set.of(element1, element2))
                 .when(roundEnvironment)
                 .getElementsAnnotatedWith(TupleSpec.class);
 
@@ -71,6 +86,10 @@ class TupleSpecProcessorTest {
         when(tupleSchemaWriter.write(eq("Tuple code for Tuple4"), eq("com.aparigraha.tuples.Tuple4"), any()))
                 .thenReturn(true);
         when(tupleSchemaWriter.write(eq("Tuple code for Tuple2"), eq("com.aparigraha.tuples.Tuple2"), any()))
+                .thenReturn(true);
+        when(tupleSchemaWriter.write(eq("Tuple code for Tuple3"), eq("com.aparigraha.tuples.Tuple3"), any()))
+                .thenReturn(true);
+        when(tupleSchemaWriter.write(eq("Tuple code for Tuple1"), eq("com.aparigraha.tuples.Tuple1"), any()))
                 .thenReturn(true);
 
         TupleSpecProcessor tupleSpecProcessor = new TupleSpecProcessor(tupleGenerator, tupleSchemaWriter);
